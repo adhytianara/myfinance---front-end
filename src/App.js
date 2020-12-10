c46a80ac05439b2d49689fd825ff011600a75a31
Ari Nugraha <nugrahaa878@gmail.com>
2020-12-10 16:46:42 +0700

    modify homepage (test)

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My Financial App
+          My Financial App 2
         </p>
         <a
           className="App-link"
