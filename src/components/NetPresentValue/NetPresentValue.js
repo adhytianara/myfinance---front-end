390e758774e9c91a6ac0e080fcca35729ff4f731
Ari Nugraha <nugrahaa878@gmail.com>
2020-12-18 15:28:59 +0700

    implement front end in npv page

--- src/components/NetPresentValue/NetPresentValue.js
+++ src/components/NetPresentValue/NetPresentValue.js
@@ -1,12 +1,13 @@
-import "./style.css"
+import "./style.css";
 import React, { useState } from "react";
+import { Navbar, Row } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 
 function InternalRateReturn() {
   // register berfungsi untuk mendaftarkan form ke hook
   const { register, handleSubmit, errors, formState, reset } = useForm();
 
-  const [npv, setNvp] = useState(0)
+  const [npv, setNvp] = useState(0);
 
   function fetchInternalRateReturn(opts) {
     fetch(
@@ -16,9 +17,9 @@ function InternalRateReturn() {
         headers: {
           "Content-Type": "application/json",
           "Access-Control-Allow-Origin": "*",
-          "x-api-key": "KYI4RhklZf5BZvassa6Fz1rl0fmXJ7H235arS7do"
+          "x-api-key": "KYI4RhklZf5BZvassa6Fz1rl0fmXJ7H235arS7do",
         },
-        body: JSON.stringify({body: JSON.stringify(opts)}),
+        body: JSON.stringify({ body: JSON.stringify(opts) }),
       }
     )
       .then(function (response) {
@@ -26,8 +27,8 @@ function InternalRateReturn() {
       })
       .then(function (data) {
         console.log(data);
-        setNvp(data['body'])
-        console.log(data['body']);
+        setNvp(data["body"]);
+        console.log(data["body"]);
       });
   }
 
@@ -45,45 +46,96 @@ function InternalRateReturn() {
     data["outcome_5"] = parseFloat(data["outcome_5"]);
     data["project_interest"] = parseFloat(data["project_interest"]);
     data["project_cost"] = parseFloat(data["project_cost"]);
-    
+
     console.log(JSON.stringify({ body: JSON.stringify(data) }));
     fetchInternalRateReturn(data);
-
   };
 
   return (
     <div>
       <h1> Is your Business Good or Not? </h1>
       <form>
-        <label> Income year 1 * </label>
-        <input type="number" name="income_1" ref={register()} />
-        <label> Income year 2 * </label>
-        <input type="number" name="income_2" ref={register()} />
-        <label> Income year 3 * </label>
-        <input type="number" name="income_3" ref={register()} />
-        <label> Income year 4 * </label>
-        <input type="number" name="income_4" ref={register()} />
-        <label> Income year 5 * </label>
-        <input type="number" name="income_5" ref={register()} />
-        <label> Outcome year 1 *</label>
-        <input type="number" name="outcome_1" ref={register()} />
-        <label> Outcome year 2 *</label>
-        <input type="number" name="outcome_2" ref={register()} />
-        <label> Outcome year 3 *</label>
-        <input type="number" name="outcome_3" ref={register()} />
-        <label> Outcome year 4 *</label>
-        <input type="number" name="outcome_4" ref={register()} />
-        <label> Outcome year 5 *</label>
-        <input type="number" name="outcome_5" ref={register()} />
-        <label> Project Interest *</label>
-        <input type="number" name="project_interest" ref={register()} />
-        <label> Project Cost *</label>
-        <input type="number" name="project_cost" ref={register()} />
+        <h2>INCOME</h2>
+        <Row className="justify-content-center">
+          <div className="form-container">
+            <label> Income year 1 </label>
+            <br></br>
+            <input type="number" name="income_1" ref={register()} />
+          </div>
+          <div className="form-container">
+            <label> Income year 2 </label>
+            <br></br>
+            <input type="number" name="income_2" ref={register()} />
+          </div>
+          <div className="form-container">
+            <label> Income year 3 </label>
+            <br></br>
+            <input type="number" name="income_3" ref={register()} />
+          </div>
+          <div className="form-container">
+            <label> Income year 4 </label>
+            <br></br>
+            <input type="number" name="income_4" ref={register()} />
+          </div>
+          <div className="form-container">
+            <label> Income year 5 </label>
+            <br></br>
+            <input type="number" name="income_5" ref={register()} />
+          </div>
+        </Row>
+        <h2>OUTCOME</h2>
+        <Row className="justify-content-center">
+          <div className="form-container">
+            <label> Outcome year 1</label>
+            <br></br>
+            <input type="number" name="outcome_1" ref={register()} />
+          </div>
+          <div className="form-container">
+            <label> Outcome year 2</label>
+            <br></br>
+            <input type="number" name="outcome_2" ref={register()} />
+          </div>
+          <div className="form-container">
+            <label> Outcome year 3</label>
+            <br></br>
+            <input type="number" name="outcome_3" ref={register()} />
+          </div>
+          <div className="form-container">
+            <label> Outcome year 4</label>
+            <br></br>
+            <input type="number" name="outcome_4" ref={register()} />
+          </div>
+          <div className="form-container">
+            <label> Outcome year 5</label>
+            <br></br>
+            <input type="number" name="outcome_5" ref={register()} />
+          </div>
+        </Row>
+        <h2>INTEREST AND COST</h2>
+        <Row className="justify-content-center">
+          <div className="form-container">
+            <label> Project Interest</label>
+            <br></br>
+            <input type="number" name="project_interest" ref={register()} />
+          </div>
+          <div className="form-container">
+            <label> Project Cost</label>
+            <br></br>
+            <input type="number" name="project_cost" ref={register()} />
+          </div>
+        </Row>
 
-        <button className="submit-button" type="button" onClick={handleSubmit(onSubmit)}>
-          Check my Business
-        </button>
-        <h1>{npv}</h1>
+        <Row className="justify-content-center">
+          <button
+            className="submit-button"
+            type="button"
+            onClick={handleSubmit(onSubmit)}
+          >
+            Check my Business
+          </button>
+        </Row>
+
+        <h1 className="result">Your NPV : {npv}</h1>
       </form>
     </div>
   );
