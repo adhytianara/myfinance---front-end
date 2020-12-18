62c23cad40d45056aae43971ce7d235dbdfdfab4
Ari Nugraha <nugrahaa878@gmail.com>
2020-12-18 14:45:56 +0700

    finish homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Image, Row } from "react-bootstrap";
+import homeLogo from "./img/home.png";
+import "./style.css";
+import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div>
-            <h1>Homepage</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Image src={homeLogo} fluid className="home-logo" />
+
+      <Row className="justify-content-center">
+        <Link to="/netpresentvalue" className="link-container">
+          <p className="link-dalam">NPV</p>
+        </Link>
+        <Link to="/#" className="link-container">
+          <p className="link-dalam">Adit</p>
+        </Link>
+        <Link to="/#" className="link-container">
+          <p className="link-dalam">Aji</p>
+        </Link>
+        <Link to="/#" className="link-container">
+          <p className="link-dalam">Ronaldi</p>
+        </Link>
+      </Row>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
