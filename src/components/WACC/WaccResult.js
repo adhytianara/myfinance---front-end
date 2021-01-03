bc55ac058f9ed2a2c025f52aa36af81cea758caf
Ronaldi Tjaidianto <ronaldi.tjaidianto@ui.ac.id>
2021-01-03 14:28:44 +0800

    fix result presentation

--- src/components/WACC/WaccResult.js
+++ src/components/WACC/WaccResult.js
@@ -7,11 +7,11 @@ function WaccResult(props) {
         <div>
             <ProgressBar position={3} />
             <Row className="justify-content-center">
-                <h1>WACC = {props.result}% </h1>
+                <h1>WACC = {props.result*100}% </h1>
             </Row>
             <Row className="justify-content-center">
-                <h5 style={{textAlign: 'center'}}>The expected return for this investment is {Math.round(props.result*100)/100}%. <br/>
-                 This means the company is expected to pay its investor an average of ${Math.round(props.result*100)/10000} in return for every $1 funded.</h5>
+                <h5 style={{textAlign: 'center'}}>The expected return for this investment is {Math.round(props.result*10000)/100}%. <br/>
+                 This means the company is expected to pay its investor an average of ${Math.round(props.result*10000)/10000} in return for every $1 funded.</h5>
             </Row>
         </div>
     );
